d20a276c60cedbbcfe9166f096b34ed7bd6f7f7b
nadyabogorodova <nadyabogorodova@gmail.com>
2018-10-28 10:38:14 +0300

    price-list block completed

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ let videoPlayer = {};
 function onYouTubeIframeAPIReady(){
     $('#play-btn').click(() => (videoPlayer = createIframe()));
 }
+
+SC.initialize({
+    client_id: 'YOUR_CLIENT_ID'
+});
+
+var track_url = 'http://soundcloud.com/forss/flickermood';
+    SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+    console.log('oEmbed response: ', oEmbed);
+});
