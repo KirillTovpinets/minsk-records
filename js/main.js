e06f3f959f0ce4dfb30512893b75ee7b03205589
Kirill Tovpinec <kirilltovpinec@admindeMacBook-Air.local>
2018-11-06 21:13:37 +0300

    complete audio-player

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
-let videoPlayer = {};
+$(document).ready(function(){
+    let videoPlayer = {};
 
-const today = new Date();
-$('#current-year').text(today.getFullYear());
-function onYouTubeIframeAPIReady(){
-    $('#play-btn').click(() => (videoPlayer = createIframe()));
-}
+    const today = new Date();
+    $('#current-year').text(today.getFullYear());
+    function onYouTubeIframeAPIReady(){
+        $('#play-btn').click(() => (videoPlayer = createIframe()));
+    }
 
-let map = initMap();
-$('nav a[href^="#"], .scroll-top').click(function(){
-    let fromTop, target;
-    target = $(this).attr('href');
-    fromTop = $(target).offset().top;
-    $('body, html').animate({
-        scrollTop: fromTop
-    }, 1000);
-    return false;
+    let map = initMap();
+    $('nav a[href^="#"], .scroll-top').click(function(){
+        let fromTop, target;
+        target = $(this).attr('href');
+        fromTop = $(target).offset().top;
+        $('body, html').animate({
+            scrollTop: fromTop
+        }, 1000);
+        return false;
+    })
+
+    $('.play-btn').click(function(){
+        let player = $('#track-1');
+        player[0].play();
+        $(this).parent().parent().addClass('is-playing');
+    })
+
+    $('.stop-btn').click(function(){
+        let player = $('#track-1');
+        console.log(player);
+        player[0].pause();
+        $(this).parent().parent().removeClass('is-playing');
+    })
+
+    $('audio').bind('ended', function(){
+        $('.is-playing').removeClass('is-playing');
+    })
 })
-// SC.initialize({
-//     client_id: 'YOUR_CLIENT_ID'
-// });
-
-// var track_url = 'http://soundcloud.com/forss/flickermood';
-//     SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
-//     console.log('oEmbed response: ', oEmbed);
-// });
